50165cf4f86d4e45e536ff76dc54811b0a4a95b3
Laura <laura@wiese2.org>
2026-06-18 01:30:00 +0200

    improve output

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module goom
 
 go 1.26.3
 
-require golang.org/x/sys v0.45.0
+require (
+	golang.org/x/sys v0.46.0
+	golang.org/x/term v0.44.0
+)
--- logger.go
+++ logger.go
@@ -4,6 +4,9 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"time"
+
+	"golang.org/x/term"
 )
 
 type LogReason uint8
@@ -15,6 +18,10 @@ const (
 	ReasonRapidGrowth
 )
 
+const targetProcessNameWidth = 20
+
+var useColor bool
+
 type logEvent struct {
 	message [64]byte
 	name    [32]byte
@@ -25,6 +32,10 @@ type logEvent struct {
 	nameLen int
 	pid     uint32
 	reason  LogReason
+}
+
+func init() {
+	useColor = term.IsTerminal(int(os.Stderr.Fd()))
 }
 
 func sendMsg(msgType int, text string) {
@@ -64,32 +75,64 @@ func startLogger() {
 		for ev := range logChan {
 			buf = buf[:0]
 
+			if useColor {
+				buf = append(buf, "\x1b[90m"...)
+			}
+
+			buf = time.Now().AppendFormat(buf, "2006-01-02 15:04:05")
+
+			if useColor {
+				buf = append(buf, "\x1b[0m"...)
+			}
+
+			buf = append(buf, " ["...)
+
 			switch ev.msgType {
 			case 0:
-				buf = append(buf, "[INFO] "...)
+				buf = append(buf, "INFO"...)
 			case 1:
-				buf = append(buf, "[WARN] "...)
+				if useColor {
+					buf = append(buf, "\x1b[33m"...)
+				}
+
+				buf = append(buf, "WARN"...)
 			case 2:
-				buf = append(buf, "[KILL] "...)
+				if useColor {
+					buf = append(buf, "\x1b[31m"...)
+				}
+
+				buf = append(buf, "KILL"...)
+			default:
+				if useColor {
+					buf = append(buf, "\x1b[94m"...)
+				}
+
+				buf = append(buf, "????"...)
 			}
+
+			if useColor {
+				buf = append(buf, "\x1b[0m"...)
+			}
+
+			buf = append(buf, "] "...)
 
 			if ev.msgLen > 0 {
 				buf = append(buf, ev.message[:ev.msgLen]...)
 			} else {
-				buf = append(buf, "PID "...)
-				buf = strconv.AppendUint(buf, uint64(ev.pid), 10)
+				buf = appendUintPadded(buf, uint64(ev.pid), 7)
+				buf = append(buf, "/ "...)
 
 				if ev.nameLen > 0 {
-					buf = append(buf, " ("...)
-					buf = append(buf, ev.name[:ev.nameLen]...)
-					buf = append(buf, ')')
+					buf = appendProcessName(buf, ev.name[:ev.nameLen])
+				} else {
+					buf = appendProcessName(buf, []byte("unknown"))
 				}
 
 				buf = append(buf, " | Mem: "...)
-				buf = strconv.AppendUint(buf, ev.bytes/(1<<20), 10)
+				buf = appendUintPadded(buf, ev.bytes/(1<<20), 5)
 				buf = append(buf, " MB | Commit: "...)
-				buf = strconv.AppendFloat(buf, ev.commit*100, 'f', 1, 64)
-				buf = append(buf, "% | "...)
+				buf = appendCommitPadded(buf, ev.commit*100, 6)
+				buf = append(buf, " | "...)
 
 				switch ev.reason {
 				case ReasonRogue:
@@ -116,4 +159,48 @@ func startLogger() {
 			}
 		}
 	}()
+}
+
+func appendUintPadded(buf []byte, val uint64, width int) []byte {
+	var tmp [20]byte
+
+	b := strconv.AppendUint(tmp[:0], val, 10)
+	buf = append(buf, b...)
+
+	for i := len(b); i < width; i++ {
+		buf = append(buf, ' ')
+	}
+
+	return buf
+}
+
+func appendCommitPadded(buf []byte, val float64, width int) []byte {
+	var tmp [24]byte
+
+	b := strconv.AppendFloat(tmp[:0], val, 'f', 1, 64)
+	buf = append(buf, b...)
+	buf = append(buf, '%')
+
+	for i := len(b) + 1; i < width; i++ {
+		buf = append(buf, ' ')
+	}
+
+	return buf
+}
+
+func appendProcessName(buf []byte, name []byte) []byte {
+	n := len(name)
+	if n > targetProcessNameWidth {
+		buf = append(buf, name[:8]...)
+		buf = append(buf, "..."...)
+		buf = append(buf, name[n-9:]...)
+	} else {
+		buf = append(buf, name...)
+
+		for i := n; i < targetProcessNameWidth; i++ {
+			buf = append(buf, ' ')
+		}
+	}
+
+	return buf
 }
